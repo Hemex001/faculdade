d29d37531d32b892c0edad3f8a27d446a2ee240a
Hemex001 <83310574+Hemex001@users.noreply.github.com>
2024-05-08 20:51:54 -0300

    Update
    Incluso TDE-06,
    update projeto (aula 7)

--- Programação Web Back-end 1 (Sistemas-6)/Project/index.js
+++ Programação Web Back-end 1 (Sistemas-6)/Project/index.js
@@ -1,8 +1,32 @@
 /* Configurando/Inicializando um servidor com express */
 const express = require("express")
+const { router } = require("./routes/movies")
 const server = express()
+server.use(express.json())
 // http://localhost:8080/
 
+
+
+server.get("/health", (req, res) => {
+    res.json({
+        status: "Running"
+    })
+})
+
+server.use("/api", router) 
+
+const port = 8080
+server.listen(port, () => {
+    console.log(`Servidor rodando na porta ${port}`)
+})
+
+
+
+
+
+
+
+/*
 server.get("/", (req, res) => {
     res.send("Hello World")
 })
@@ -23,13 +47,10 @@ server.get("/user", (req, res) => {
     })
 })
 
-const port = 8080
-server.listen(port, () => {
-    console.log(`Servidor rodando na porta ${port}`)
-})
-
-
-
-
-
-//console.log("Hello World")
+// Aula 08/05
+npm install prisma --save-dev
+npx prisma init --datasource-provider sqlite
+npx prisma migrate dev --name "create movies table"
+npm install prisma --save-dev 
+npx prisma studio  // Abre a interface do banco de dados no navegador
+*/
---
+++ Programação Web Back-end 1 (Sistemas-6)/Project/routes/movies.js
@@ -0,0 +1,41 @@
+const express = require("express")
+const { findMovies, findMoviesById, createMovie, updateMovie, deleteMovie } = require("../db/movies")
+const router = express.Router() // criando um novo roteador dentro do projeto
+
+//http://localhost:8080/api/movies?rate=10
+//http://localhost:8080/api/movies
+// selecionar todos
+router.get("/movies", async (req, res) => {
+    const movies = await findMovies(req.query.rate)
+    res.json(movies)
+})
+//selecionar especificp
+router.get("/movie/:id", async (req, res) => {
+    const id = Number(req.params.id)
+    const movie = await findMoviesById(id)
+    if (!movie) return res
+        .status(404).json({message: `Movie with id ${id} not found.`})
+    res.json(movie)
+})
+// criar
+router.post("/movie", async (req, res) => {
+    const data = req.body
+    const movie = await createMovie(data)
+    res.status(201).json(movie)
+})
+// atualizar
+router.put("/movie/:id", async (req, res) => {
+    const id = Number(req.params.id)
+    const movie = await updateMovie(id, req.body)
+    res.json(movies)
+})
+// deletar
+router.delete("/movie/:id", async (req, res) => {
+    const id = Number(req.params.id)
+    await deleteMovie(id)
+    res.status(204).send()
+})
+
+module.exports = { 
+    router
+}
